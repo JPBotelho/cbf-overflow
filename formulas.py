6ed0d1240e75241536426efcdbe9456899204dde
JPBotelho <jpbotelho.costa@gmail.com>
2024-01-19 22:49:03 +0000

    changed math.comb to gmpy2.bincoef

--- formulas.py
+++ formulas.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 from fractions import Fraction
-from math import perm, comb
-from gmpy2 import mpz
+from math import perm
+from gmpy2 import mpz, bincoef
 
 
 def exact_ovf_rate(n, m, k, h):
@@ -11,7 +11,7 @@ def exact_ovf_rate(n, m, k, h):
     stirling[1+j, 1] = -1
 
     for row in range(1, nk-j):
-        comb_ = comb(row+j, j)
+        comb_ = bincoef(row+j, j)
         for col in range(1, row+1):
             val = stirling[row, col]
             stirling[row+1,   col] += val * col
@@ -33,7 +33,7 @@ def exact_ovf_rate(n, m, k, h):
 
 def upper_bound(n, m, k, h):
     def p(val):
-        return comb(n*k, val) * (1 - 1/m)**(n*k-val) * (1 / m)**val
+        return bincoef(n*k, val) * (1 - 1/m)**(n*k-val) * (1 / m)**val
 
     j = 2**h
     a = (n * k) / (m-1)
